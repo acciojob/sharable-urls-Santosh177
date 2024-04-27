6ec248070c2d3b463d1fe68c1d7e65f9094b51a5
Santosh177 <117721403+Santosh177@users.noreply.github.com>
2024-04-27 08:25:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+const btn = document.getElementById("button");
+
+const handleurl = () => {
+    const name = document.getElementById("name").value;
+    const year = document.getElementById("year").value;
+    const url = document.getElementById("url");
+
+    // Update the text of the h3 element with the appropriate query string
+    url.textContent = `https://localhost:8080/?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+};
+
+btn.addEventListener("click", handleurl);
